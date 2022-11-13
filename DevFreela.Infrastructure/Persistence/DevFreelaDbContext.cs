628c862c91b6cad3750d0577f426d598a359526c
Renan Martins <renanmartins1999@hotmail.com>
2022-11-13 20:59:35 -0300

    [FEAT] Adicionando DbContext e DbSets

--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -4,38 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.Infrastructure.Persistence
 {
-    public class DevFreelaDbContext
+    public class DevFreelaDbContext : DbContext
     {
-        public DevFreelaDbContext()
+        public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : base(options)
         {
-            Projects = new List<Project>
-            {
-                new Project("Projeto ASP.NET Core 1", "Testando DbContext com o projeto 1", 1, 1, 10000),
-                new Project("Projeto ASP.NET Core 2", "Testando DbContext com o projeto 2", 2, 2, 20000),
-                new Project("Projeto ASP.NET Core 3", "Testando DbContext com o projeto 3", 3, 3, 30000)
-            };
 
-            Users = new List<User>
-            {
-                new User("Renan Martins Mendes da Silva", "email1@hotmail.com", new DateTime(1999, 12, 4)),
-                new User("Matheus José da Silva", "email2@hotmail.com", new DateTime(2000, 12, 20)),
-                new User("Felipe Silva da Cunha", "email3@hotmail.com", new DateTime(1998, 4, 3))
-            };
-
-            Skills = new List<Skill>
-            {
-                new Skill("C#, .NET 6, ASP.NET Core"),
-                new Skill("HTML, CSS, JavaScript"),
-                new Skill("HTML, CSS, PHP")
-
-            };
         }
-        public List<Project> Projects { get; set; }
-        public List<Skill> Skills { get; set; }
-        public List<User> Users { get; set; }
-        public List<ProjectComment> ProjectComments { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
     }
 }
