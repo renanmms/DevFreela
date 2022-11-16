f09ee8932a12902fe43da0f199f7280a64a0cc4c
Renan Martins <renanmartins1999@hotmail.com>
2022-11-16 20:19:36 -0300

    [FEAT] Adicionando string de conexão

--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -2,6 +2,7 @@ using DevFreela.API.Models;
 using DevFreela.Application.Services.Implementations;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 //builder.Services.AddScoped<ExampleClass>(e => new ExampleClass { Name = "Initial Stage"});
 
-builder.Services.AddSingleton<DevFreelaDbContext>();
+var connectionString = builder.Configuration.GetConnectionString("DevFreelaCs");
+builder.Services.AddDbContext<DevFreelaDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddScoped<IProjectService, ProjectService>();
 builder.Services.AddScoped<IUserService, UserService>();
