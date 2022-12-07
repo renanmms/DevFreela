465df24f62818a00f0be09055adb4b7ff49bbb60
Renan Martins <renanmartins1999@hotmail.com>
2022-12-06 22:43:52 -0300

    [FEAT] Realizando operações com Dapper

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -1,9 +1,12 @@
-﻿using DevFreela.Application.InputModels;
+﻿using Dapper;
+using DevFreela.Application.InputModels;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.Core.Entities;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,10 +18,14 @@ namespace DevFreela.Application.Services.Implementations
     public class ProjectService : IProjectService
     {
         private readonly DevFreelaDbContext _dbContext;
-        public ProjectService(DevFreelaDbContext dbContext)
+        private readonly string _connectionString;
+
+        public ProjectService(DevFreelaDbContext dbContext, IConfiguration configuration)
         {
-            _dbContext = dbContext; 
+            _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
         }
+
         public int Create(NewProjectInputModel inputModel)
         {
             var project = new Project(inputModel.Title, inputModel.Description, inputModel.IdClient, inputModel.IdFreelancer, inputModel.TotalCost);
@@ -86,7 +93,25 @@ namespace DevFreela.Application.Services.Implementations
         {
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
             project.Start();
-            _dbContext.SaveChanges();
+            //_dbContext.SaveChanges();
+
+            using(var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                var script = @"UPDATE 
+                                    Projects
+                               SET
+                                    Status = @status,
+                                    StartedAt = @startedAt
+                               WHERE
+                                    Id = @id";
+
+                sqlConnection.Execute(script, 
+                    new { status = project.Status,
+                          startedat = project.StartedAt,
+                          id });
+            }
         }
 
         // Irá ser removido quando configurarmos o EF Core
--- DevFreela.Application/Services/Implementations/SkillService.cs
+++ DevFreela.Application/Services/Implementations/SkillService.cs
@@ -1,8 +1,12 @@
-﻿using DevFreela.Application.Services.Interfaces;
+﻿using Dapper;
+using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +16,24 @@ namespace DevFreela.Application.Services.Implementations
     public class SkillService : ISkillService
     {
         private readonly DevFreelaDbContext _dbContext;
+        private readonly string _connectionString;
 
-        public SkillService(DevFreelaDbContext dbContext)
+        public SkillService(DevFreelaDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
         }
 
         public List<SkillViewModel> GetAll()
         {
-            var skills = _dbContext.Skills;
+            using(var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
 
-            var skillsViewModel = skills.Select(s => new SkillViewModel(s.Id, s.Description)).ToList();
-            
-            return skillsViewModel;
+                var script = @"SELECT Id, Description FROM Skills";
+
+                return sqlConnection.Query<SkillViewModel>(script).ToList();
+            }
         }
     }
 }
