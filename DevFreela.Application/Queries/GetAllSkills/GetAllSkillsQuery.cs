6f11aa7054e3d52ef16239e06a699e76ab32d38c
Renan Martins <renanmartins1999@hotmail.com>
2023-01-11 22:40:04 -0300

    [FEAT] Adicionando DTO para a Skill
    - Adicionando DTO para a Skill e configurando a Query

--- DevFreela.Application/Queries/GetAllSkills/GetAllSkillsQuery.cs
+++ DevFreela.Application/Queries/GetAllSkills/GetAllSkillsQuery.cs
@@ -1,11 +1,12 @@
 ﻿using DevFreela.Application.ViewModels;
+using DevFreela.Core.DTOs;
 using DevFreela.Core.Entities;
 using MediatR;
 
 namespace DevFreela.Application.Queries.GetAllSkills
 {
 
-    public class GetAllSkillsQuery : IRequest<List<SkillViewModel>>
+    public class GetAllSkillsQuery : IRequest<List<SkillDTO>>
     {
     }
 }
