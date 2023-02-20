b2fe3c06aa769640974c44acc0fbe1b602494da7
Renan Martins <renanmartins1999@hotmail.com>
2023-02-19 23:34:36 -0300

    [FIX] Ajustando configuração do FluentValidation
    Foi adicionando o pacote do FluentValidation.AspNetCore na camada de API

--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -5,7 +5,8 @@ using DevFreela.Application.Commands.CreateProject;
 using DevFreela.Application.Queries.GetAllSkills;
 using DevFreela.Core.Repositories;
 using DevFreela.Infrastructure.Repositories;
-using FluentValidation;
+using FluentValidation.AspNetCore;
+using DevFreela.Application.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +15,7 @@ var connectionString = builder.Configuration.GetConnectionString("DevFreelaCs");
 builder.Services.AddDbContext<DevFreelaDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers()
-    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateUserCommandValidator>);
+    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateUserCommandValidator>());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
