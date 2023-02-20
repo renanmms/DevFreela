b4ed29b98bc91216ffdf6f5fd4c51c20d2056cd2
Renan Martins <renanmartins1999@hotmail.com>
2023-02-19 23:05:10 -0300

    [FEAT] Adicionando propriedade de senha no comando

--- DevFreela.Application/Commands/CreateUser/CreateUserCommand.cs
+++ DevFreela.Application/Commands/CreateUser/CreateUserCommand.cs
@@ -12,5 +12,6 @@ namespace DevFreela.Application.Commands.CreateUser
         public string FullName { get; set; }
         public string Email { get; set; }
         public DateTime BirthDate { get; set; }
+        public string Password { get; set; }
     }
 }
