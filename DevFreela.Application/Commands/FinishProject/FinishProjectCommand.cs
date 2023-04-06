71f66bab383e1ff7107a76ea238840f99ccd81b9
Renan Martins <renanmartins1999@hotmail.com>
2023-04-06 20:18:35 -0300

    [FEAT] Adicionando propiedades no comando de finalizar projeto

--- DevFreela.Application/Commands/FinishProject/FinishProjectCommand.cs
+++ DevFreela.Application/Commands/FinishProject/FinishProjectCommand.cs
@@ -11,5 +11,9 @@ namespace DevFreela.Application.Commands.FinishProject
     public class FinishProjectCommand : IRequest<ProjectStatusEnum>
     {
         public int Id { get; set; }
+        public string CreditCardNumber { get; set; }
+        public string Cvv { get; set; }
+        public string ExpiresAt { get; set; }
+        public string FullName { get; set; }
     }
 }
