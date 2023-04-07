c4c26fdd68f247c6e5947b9808686fb71b755110
Renan Martins <renanmartins1999@hotmail.com>
2023-04-07 10:27:48 -0300

    [FEAT] Adicionando construtor do DTO de pagamento

--- DevFreela.Core/DTOs/PaymentInfoDTO.cs
+++ DevFreela.Core/DTOs/PaymentInfoDTO.cs
@@ -2,6 +2,15 @@ namespace DevFreela.Core.DTOs
 {
     public class PaymentInfoDTO
     {
+        public PaymentInfoDTO(int id, string fullName, string cvv, string creditCardNumber, DateTime expiresAt)
+        {
+            Id = id;
+            FullName = fullName;
+            Cvv = cvv;
+            CreditCardNumber = creditCardNumber;
+            ExpiresAt = expiresAt;
+        }
+
         public int Id { get; set; }        
         public string FullName { get; set; }
         public string Cvv { get; set; }
