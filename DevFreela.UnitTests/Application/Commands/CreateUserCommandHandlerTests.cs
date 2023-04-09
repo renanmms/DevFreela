57df54f8070f04c3c0f693a32bea5fbae2b1c765
Renan Martins <renanmartins1999@hotmail.com>
2023-04-08 22:00:21 -0300

    [TEST] Adicionando teste para criação de usuário

---
+++ DevFreela.UnitTests/Application/Commands/CreateUserCommandHandlerTests.cs
@@ -0,0 +1,34 @@
+using DevFreela.Application.Commands.CreateUser;
+using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
+using Moq;
+
+namespace DevFreela.UnitTests.Application.Commands
+{
+    public class CreateUserCommandHandlerTests
+    {
+        [Fact]
+        public async Task LoginData_Executed_UserId(){
+
+            // Arrange
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var authServiceMock = new Mock<IAuthService>();
+
+            var command = new CreateUserCommand{
+                FullName = "Renan Martins",
+                Email = "renanmartins@email.com",
+                BirthDate = DateTime.Now,
+                Password = "renan1234",
+                Role = "freelancer"
+            };
+
+            var createUserCommandHandler = new CreateUserCommandHandler(userRepositoryMock.Object, authServiceMock.Object);
+
+            // Act
+            var id = await createUserCommandHandler.Handle(command, new CancellationToken());
+
+            // Assert
+            Assert.NotNull(id);
+        }
+    }
+}
