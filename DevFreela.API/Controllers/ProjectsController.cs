b4d5183b5b33913765b82d249e618fcad100ec8d
Renan Martins <renanmartins1999@hotmail.com>
2024-07-29 23:08:12 -0300

    [FEAT] Implementando padrão IOptions

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,5 +1,6 @@
 ﻿using DevFreela.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers
 {
@@ -7,6 +8,12 @@ namespace DevFreela.API.Controllers
     [Route("api/[controller]")]
     public class ProjectsController : ControllerBase
     {
+        private readonly FreelanceTotalCostConfig _config;
+        public ProjectsController(IOptions<FreelanceTotalCostConfig> options)
+        {
+            _config = options.Value;
+        }
+
         // GET api/projects?search=crm
         [HttpGet]
         public IActionResult Get(string searchs)
@@ -24,6 +31,11 @@ namespace DevFreela.API.Controllers
         [HttpPost]
         public IActionResult Post(CreateProjectInputModel model)
         {
+            if (model.TotalCost < _config.Minimum || model.TotalCost > _config.Maximum)
+            {
+                return BadRequest("Numero fora dos limites.");
+            }
+
             return CreatedAtAction(nameof(GetById), new { id = 1}, model);
         }
 
@@ -55,6 +67,7 @@ namespace DevFreela.API.Controllers
         public IActionResult PostComment(CreateProjectCommentInputModel model)
         {
             return NoContent();
+
         }
 
     }
