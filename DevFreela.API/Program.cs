0a618d06958bc9b85ba0e54fc56aa0409846d5ff
Renan Martins <renanmartins1999@hotmail.com>
2024-07-30 21:41:43 -0300

    [FEAT] Injeção de dependência do ConfigService

--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -1,4 +1,5 @@
 using DevFreela.API.Models;
+using DevFreela.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<FreelanceTotalCostConfig>(builder.Configuration.GetSection(nameof(FreelanceTotalCostConfig)));
+builder.Services.AddTransient<IConfigService, ConfigService>();
 
 var app = builder.Build();
 
