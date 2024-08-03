38607d3ffa2389489f917b338ad5c8bc7d6e5642
Renan Martins <renanmartins1999@hotmail.com>
2024-08-02 22:25:09 -0300

    [FEAT] Criando entidades

---
+++ DevFreela.API/Entities/Skill.cs
@@ -0,0 +1,12 @@
+namespace DevFreela.API.Entities
+{
+    public class Skill : BaseEntity
+    {
+        public Skill(string description)
+        {
+            Description = description;
+        }
+        
+        public string Description { get; private set; }
+    }
+}
---
+++ DevFreela.API/Enums/ProjectStatusEnum.cs
@@ -0,0 +1,13 @@
+namespace DevFreela.API.Enums
+{
+    public enum ProjectStatusEnum
+    {
+        Created = 0,
+        Started = 1,
+        Suspended = 2,
+        InProgress = 3,
+        Canceled = 4,
+        Completed = 5,
+        PaymentPending = 6
+    }
+}
