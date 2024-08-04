97f4640e8c801e35002f11153431bc2d0bcd6b38
Renan Martins <renanmartins1999@hotmail.com>
2024-08-04 10:04:55 -0300

    [REFACTOR] Removendo serviço desnecessário

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -9,36 +9,29 @@ namespace DevFreela.API.Controllers
     [Route("api/[controller]")]
     public class ProjectsController : ControllerBase
     {
-        private readonly FreelanceTotalCostConfig _config;
-        private readonly IConfigService _configService;
-        public ProjectsController(IOptions<FreelanceTotalCostConfig> options, IConfigService configService)
+
+        public ProjectsController()
         {
-            _config = options.Value;
-            _configService = configService;
+
         }
 
         // GET api/projects?search=crm
         [HttpGet]
         public IActionResult Get(string searchs)
         {
-            return Ok(_configService.GetValue());
+            return Ok();
         }
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id) 
         {
-            return Ok(_configService.GetValue());
+            return Ok();
         }
 
 
         [HttpPost]
         public IActionResult Post(CreateProjectInputModel model)
         {
-            if (model.TotalCost < _config.Minimum || model.TotalCost > _config.Maximum)
-            {
-                return BadRequest("Numero fora dos limites.");
-            }
-
             return CreatedAtAction(nameof(GetById), new { id = 1}, model);
         }
 
