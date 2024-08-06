737cc8af37e4242d57fc87bd79cbf2deb86045df
Renan Martins <renanmartins1999@hotmail.com>
2024-08-06 07:32:59 -0300

    [FEAT] Injeção do Context no ProjectsController

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,7 +1,9 @@
-﻿using DevFreela.API.Models;
+﻿using DevFreela.API.Entities;
+using DevFreela.API.Models;
 using DevFreela.API.Persistence;
 using DevFreela.API.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers
@@ -18,51 +20,122 @@ namespace DevFreela.API.Controllers
 
         // GET api/projects?search=crm
         [HttpGet]
-        public IActionResult Get(string searchs)
+        public IActionResult Get(string searchs = "")
         {
-            return Ok();
+            var projects = _context.Projects
+                .Include(p => p.Client)
+                .Include(p => p.Freelancer)
+                .Include(p => p.Comments)
+                .Where(p => !p.IsDeleted);
+            var model = projects.Select(ProjectItemViewModel.FromEntity);
+
+            return Ok(model);
         }
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id) 
         {
-            return Ok();
+            var project = _context.Projects
+                .Include(p => p.Client)
+                .Include(p => p.Freelancer)
+                .Include(p => p.Comments)
+                .SingleOrDefault(p => p.Id == id);
+
+            var model = ProjectItemViewModel.FromEntity(project);
+
+            return Ok(model);
         }
 
 
         [HttpPost]
         public IActionResult Post(CreateProjectInputModel model)
         {
-            return CreatedAtAction(nameof(GetById), new { id = 1}, model);
+            var project = model.ToEntity();
+
+            _context.Projects.Add(project);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById), new { id = project.Id}, model);
         }
 
         [HttpPut("{id}")]
         public IActionResult Put(int id, UpdateProjectInputModel model)
         {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+            if(project is null)
+            {
+                return NotFound();
+            }
+
+            project.Update(model.Title, model.Description, model.TotalCost);
+            _context.Projects.Update(project);
+            _context.SaveChanges();
+
             return NoContent();
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+            if(project is null)
+            {
+                return NotFound();
+            }
+
+            project.SetAsDeleted();
+            _context.Projects.Update(project);
+            _context.SaveChanges();
+
             return NoContent();
         }
 
         [HttpPut("{id}/start")]
         public IActionResult Start(int id)
         {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+            if(project is null)
+            {
+                return NotFound();
+            }
+
+            project.Start();
+            _context.Projects.Update(project);
+            _context.SaveChanges();
+
             return NoContent();
         }
 
         [HttpPut("{id}/complete")]
         public IActionResult Complete(int id)
         {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+            if(project is null)
+            {
+                return NotFound();
+            }
+
+            project.Complete();
+            _context.Projects.Update(project);
+            _context.SaveChanges();
+
             return NoContent();
         }
 
         [HttpPost("{id}/comments")]
         public IActionResult PostComment(CreateProjectCommentInputModel model)
         {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == model.IdProject);
+            if(project is null)
+            {
+                return NotFound();
+            }
+
+            var comment = new ProjectComment(model.Content, model.IdProject, model.IdUser);
+            
+            _context.Comments.Add(comment);
+            _context.SaveChanges();
+
             return NoContent();
 
         }
