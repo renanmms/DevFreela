1f5ac6dfb5a97c6e29ae994556fa632f41e66b62
Renan Martins <renanmartins1999@hotmail.com>
2024-08-06 21:29:37 -0300

    [FEAT] Injeção do Context em UsersController

--- DevFreela.API/Controllers/UsersController.cs
+++ DevFreela.API/Controllers/UsersController.cs
@@ -1,19 +1,56 @@
-﻿using DevFreela.API.Models;
+﻿using DevFreela.API.Entities;
+using DevFreela.API.Models;
+using DevFreela.API.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.API.Controllers
 {
     public class UsersController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult Post()
+        private readonly DevFreelaDbContext _context;
+        public UsersController(DevFreelaDbContext context)
         {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = _context.Users
+                .Include(u => u.Skills)
+                    .ThenInclude(u => u.Skill)
+                .SingleOrDefault(u => u.Id == id);
+
+            if(user is null)
+            {
+                return NotFound();
+            }
+
+            var model = UserViewModel.FromEntity(user);
+            
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public IActionResult Post(CreateUserInputModel model)
+        {
+            var user = new User(model.FullName, model.Email, model.Birthdate);
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
             return Ok();
         }
 
         [HttpPost("{id}/skills")]
-        public IActionResult PostSkills(UserSkillsInputModel model)
+        public IActionResult PostSkills(int id, UserSkillsInputModel model)
         {
+            var userSkills = model.SkillIds.Select(s => new UserSkill(id, s)).ToList();
+
+            _context.UserSkills.AddRange(userSkills);
+            _context.SaveChanges();
+
             return NoContent();
         }
 
