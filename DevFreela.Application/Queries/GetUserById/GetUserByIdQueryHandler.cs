a9220d3639e811543206c28ca4c248ea5b7e4777
Renan Martins <renanmartins1999@hotmail.com>
2024-09-24 21:56:32 -0300

    [FEAT] Query para obter usuário por Id

---
+++ DevFreela.Application/Queries/GetUserById/GetUserByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+using DevFreela.Application.Models;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevFreela.Application.Queries.GetUserById
+{
+    public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, ResultViewModel<UserViewModel>>
+    {
+        private readonly DevFreelaDbContext _context;
+        public GetUserByIdQueryHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<UserViewModel>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users
+                .Include(u => u.Skills)
+                    .ThenInclude(u => u.Skill)
+                .SingleOrDefaultAsync(u => u.Id == request.Id);
+
+            if(user is null)
+            {
+                return ResultViewModel<UserViewModel>.Error("User not found");
+            }
+
+            var model = UserViewModel.FromEntity(user);
+            
+            return ResultViewModel<UserViewModel>.Success(model);
+        }
+    }
+}
