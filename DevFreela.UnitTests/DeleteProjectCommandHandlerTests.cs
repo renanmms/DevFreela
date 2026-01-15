f3994ce53a0cc8ae73e8b9f1e182fa834a4837c2
Renan Martins <renanmartins1999@hotmail.com>
2026-01-15 19:52:48 -0300

    [TEST] Testes unitários com Moq

--- DevFreela.UnitTests/DeleteProjectCommandHandlerTests.cs
+++ DevFreela.UnitTests/DeleteProjectCommandHandlerTests.cs
@@ -1,6 +1,7 @@
 using DevFreela.Application.Commands.DeleteProject;
 using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
+using Moq;
 using NSubstitute;
 
 namespace DevFreela.UnitTests;
@@ -28,6 +29,28 @@ public class DeleteProjectCommandHandlerTests
         await repository.Received(1).GetByIdAsync(Arg.Any<int>());
         await repository.Received(1).UpdateAsync(Arg.Any<Project>());
     }
+    
+    [Fact]
+    public async Task ProjectExists_Delete_Success_Moq() 
+    {
+        // Arrange
+        const int ID = 1;
+        var project = new Project("Project A", "Project Description", 1, 2, 20000);
+        var repository = Mock.Of<IProjectRepository>(p => 
+            p.GetByIdAsync(It.IsAny<int>()) == Task.FromResult(project) &&
+            p.UpdateAsync(It.IsAny<Project>()) == Task.CompletedTask);
+        
+        var handler = new DeleteProjectCommandHandler(repository);
+        var command = new DeleteProjectCommand(ID);
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+        
+        // Assert
+        Assert.True(result.IsSuccess);
+        Mock.Get(repository).Verify(m => m.GetByIdAsync(It.IsAny<int>()), Times.Once);
+        Mock.Get(repository).Verify(m => m.UpdateAsync(It.IsAny<Project>()), Times.Once);
+    }
 
     [Fact]
     public async Task ProjectDoesExists_Delete_Error_NSubstitute()
@@ -47,6 +70,26 @@ public class DeleteProjectCommandHandlerTests
         Assert.False(result.IsSuccess);
         await repository.Received(1).GetByIdAsync(Arg.Any<int>());
         await repository.DidNotReceive().UpdateAsync(Arg.Any<Project>());
+        Assert.Equal(DeleteProjectCommandHandler.PROJECT_NOT_FOUND_MESSAGE, result.Message);
+    }
+    
+    [Fact]
+    public async Task ProjectDoesNotExists_Delete_Error_Moq()
+    {
+        // Arrange
+        const int ID = 1;
+        var repository = Mock.Of<IProjectRepository>(r => r.GetByIdAsync(It.IsAny<int>()) == Task.FromResult((Project?)null));
+        
+        var handler = new DeleteProjectCommandHandler(repository);
+        var command = new DeleteProjectCommand(ID);
+        
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+        
+        // Assert
+        Assert.False(result.IsSuccess);
+        Mock.Get(repository).Verify(r => r.GetByIdAsync(It.IsAny<int>()), Times.Once);
+        Mock.Get(repository).Verify(r => r.UpdateAsync(It.IsAny<Project>()), Times.Never);
         Assert.Equal(DeleteProjectCommandHandler.PROJECT_NOT_FOUND_MESSAGE, result.Message);
     }
 }
--- DevFreela.UnitTests/InsertProjectCommandHandlerTests.cs
+++ DevFreela.UnitTests/InsertProjectCommandHandlerTests.cs
@@ -1,7 +1,9 @@
 using DevFreela.Application.Commands.InsertProject;
+using DevFreela.Application.Notifications.ProjectCreated;
 using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
 using MediatR;
+using Moq;
 using NSubstitute;
 
 namespace DevFreela.UnitTests;
@@ -29,5 +31,25 @@ public class InsertProjectCommandHandlerTests
         Assert.True(result.IsSuccess);
         Assert.Equal(ID, result.Data);
         await repository.Received(1).AddAsync(Arg.Any<Project>());
+    }
+    
+    [Fact]
+    public async Task InputDataAreOk_Insert_Success_Moq()
+    {
+        // Arrange
+        const int ID = 1;
+        var repository = Mock.Of<IProjectRepository>(p => p.AddAsync(It.IsAny<Project>()) == Task.FromResult(ID));
+        var mediator = Mock.Of<IMediator>(m => m.Publish(It.IsAny<ProjectCreatedNotification>(), CancellationToken.None) == Task.CompletedTask);
+
+        var command = new InsertProjectCommand("Project Title", "Project Description", 1, 2, 20000);
+        var handler = new InsertProjectCommandHandler(repository, mediator);
+        
+        // Act
+        var result = await handler.Handle(command, new CancellationToken());
+        
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(ID, result.Data);
+        Mock.Get(repository).Verify(p => p.AddAsync(It.IsAny<Project>()), Times.Once);
     }
 }
