f35d176273f1187e32dba1216cba2101a5c22e84
Renan Martins <renanmartins1999@hotmail.com>
2026-04-04 11:22:49 -0300

    feat: use primary constructor + idClient property

--- DevFreela.Application/Models/ProjectItemViewModel.cs
+++ DevFreela.Application/Models/ProjectItemViewModel.cs
@@ -2,28 +2,18 @@ using DevFreela.Core.Entities;
 
 namespace DevFreela.Application.Models
 {
-    public class ProjectItemViewModel
+    public record ProjectItemViewModel(int Id,
+            int IdClient,
+            string Title,
+            string ClientName,
+            string FreelancerName,
+            decimal TotalCost,
+            string Description)
     {
-        public ProjectItemViewModel(int id, string title, string clientName, string freelancerName, decimal totalCost, string description)
-        {
-            Id = id;
-            Title = title;
-            ClientName = clientName;
-            FreelancerName = freelancerName;
-            TotalCost = totalCost;
-            Description = description;
-        }
-
-        public int Id { get; private set; }
-        public string Title { get; private set; }
-        public string ClientName { get; private set; }
-        public string FreelancerName { get; private set; }
-        public decimal TotalCost { get; private set; }
-        public string Description { get; set; }
-
         public static ProjectItemViewModel FromEntity(Project project)
         {
             return new(project.Id,
+             project.IdClient,
              project.Title,
              project.Client.FullName,
              project.Freelancer.FullName,
